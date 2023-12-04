ccf00d76f023b4ba0e08d4c75bb0794b7c70dc8b
juliegip <julie.gip@gmail.com>
2023-12-04 12:44:24 +0100

    change nav in sidebar

--- src/components/Template/Navigation.js
+++ src/components/Template/Navigation.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Hamburger from './Hamburger';
 import routes from '../../data/routes';
 
 // Websites Navbar, displays routes defined in 'src/data/routes'
 const Navigation = () => (
   <header id="header">
-    <h1 className="index-link">
-      {routes.filter((l) => l.index).map((l) => (
-        <Link key={l.label} to={l.path}>{l.label}</Link>
-      ))}
-    </h1>
     <nav className="links">
       <ul>
         {routes.filter((l) => !l.index).map((l) => (
@@ -21,7 +15,6 @@ const Navigation = () => (
         ))}
       </ul>
     </nav>
-    <Hamburger />
   </header>
 );
 
